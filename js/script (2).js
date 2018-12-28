22194673cc780db7b78fa204b084e7e7c554aca1
DanilPirogg <pirogg1977@gmail.com>
2018-12-28 19:46:08 +0200

    finish

---
+++ js/script (2).js
@@ -0,0 +1,25 @@
+
+
+
+$(document).ready(function(){
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:5,
+        nav:true,
+        navText: ['prev','next'],
+
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    });
+});
+
